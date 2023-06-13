1be87ed4be95b4fe05315f3ffcbba7a32d72cf66
Kuruk-Lin <kaochieh@google.com>
2023-06-13 16:16:41 +0800

    PP: cr

--- temp-dev/PP.py
+++ temp-dev/PP.py
@@ -8,15 +8,23 @@ class PPC:
         self.url = target_url
         self.visit_pass = 200
         self.raw_data = self._grv()
+        self.raw_content = self.u_content()
+        self.fo_list = {
+            "userdata": "user data"
+        }
 
     def m_proc(self):
         if self.raw_data.status_code == self.visit_pass:
             logging.info(f"Privacy Policy link access is valid.")
-        print(self.raw_data.text)
+        for no, key in enumerate(self.fo_list.keys()):
+            print(str(self.raw_content).lower().find(self.fo_list.get(key)))
+
+    def u_content(self):
+        return self.raw_data.text
 
     def _grv(self):
         return requests.get(url=self.url)
 
 
 if __name__ == '__main__':
-    PPC(target_url="").m_proc()
+    PPC(target_url="https://www.iguzzini.com/privacy-statement/").m_proc()
