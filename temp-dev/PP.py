47b11ebd113f1feeabf9c42b561b01247fe85f3d
Kuruk-Lin <kaochieh@google.com>
2023-06-13 16:04:02 +0800

    PP: cr

--- temp-dev/PP.py
+++ temp-dev/PP.py
@@ -1,21 +1,22 @@
 import requests
 import logging
+from bs4 import BeautifulSoup
 
 
 class PPC:
     def __init__(self, target_url):
         self.url = target_url
         self.visit_pass = 200
+        self.raw_data = self._grv()
 
-    def capture_desc(self):
-        raw_data = requests.get(url=self.url)
-        #   visit url
-        if raw_data.status_code == self.visit_pass:
-            logging.info(f"Privacy Policy link access is normal ({raw_data.status_code == self.visit_pass})")
-        #   get description
-        print(raw_data.text)
+    def m_proc(self):
+        if self.raw_data.status_code == self.visit_pass:
+            logging.info(f"Privacy Policy link access is valid.")
+        print(self.raw_data.text)
 
+    def _grv(self):
+        return requests.get(url=self.url)
 
 
 if __name__ == '__main__':
-    PPC(target_url="https://www.iguzzini.com/privacy-statement/").capture_desc()
+    PPC(target_url="").m_proc()
